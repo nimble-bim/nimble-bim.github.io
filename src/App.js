b841f41b066cba21f7eb362491311dcb77528180
PrestonII <smith.preston2@gmail.com>
2019-08-07 10:24:49 -0400

    adding landing page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HowItWorks from './components/pages/howitworks.page';
+import Landing from './components/pages/landing/landing.page';
 
 function App() {
   return (
-    <HowItWorks />
+    // <HowItWorks />
+    <Landing />
   );
 }
 
