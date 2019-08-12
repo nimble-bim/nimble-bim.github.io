a6f1b9ad03caf9c590a444121152d174013947ee
PrestonII <smith.preston2@gmail.com>
2019-08-12 12:49:16 -0400

    add colors and themes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import HowItWorks from './components/pages/howitworks/howitworks.page';
 import RegistrationPage from './components/pages/registration/registration.page';
+import './styles/app.index.scss';
 
 function App() {
   return (
