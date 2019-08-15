8cf22f562663a2439f64ee506506b23d11987fa5
PrestonII <smith.preston2@gmail.com>
2019-08-15 15:30:44 -0400

    added section titles and styled them

--- src/components/pages/about/about.page.js
+++ src/components/pages/about/about.page.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './about.desktop.scss';
 import './about.mobile.scss';
 
+import { SectionTitle } from '../../subcomponents/misc/organizational.object';
+
 export default class AboutPage extends React.Component {
   constructor(props) {
     super(props);
@@ -12,12 +14,47 @@ export default class AboutPage extends React.Component {
     return (
       <div className="page page__about about">
 
-        <div className="about__mission">mission</div>
-        <div className="about__content">content</div>
-        <div className="about__news">news</div>
-        <div className="about__timeline">timeline</div>
+        <div className="about__mission">
+          <SectionTitle section="Our Mission" />
+        </div>
+
+        <div className="about__contact">
+          <SectionTitle section="Contact Us" />
+        </div>
+
+        <div className="about__content">
+          <SectionTitle section="About Us" />
+        </div>
+
+        <div className="about__news">
+          <SectionTitle section="Recent News" />
+        </div>
+        <div className="about__timeline">
+          <SectionTitle section="Timeline" />
+        </div>
 
       </div>
     )
   }
 }
+
+export const ContactList = (props) => {
+  const contact_list = props.contacts 
+    ? props.contacts.map(c => <Contact contact_type={props.contact_type} contact_info={props.contact_info} /> )
+    : <li></li>
+
+  return (
+    <div className="contacts">
+      <ul className="contacts__list">
+        { contact_list }
+      </ul>
+    </div>
+  )
+}
+
+export const Contact = (props) => (
+  <div className="contact">
+    <span className="contact__type">{props.contact_type}</span>
+    <span className="contact__info">{props.contact_info}</span>
+  </div>
+)
