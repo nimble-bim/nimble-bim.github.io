de31f4dcb6462bfeb1b02cdae160b030b3796b9b
PrestonII <smith.preston2@gmail.com>
2019-08-16 11:00:34 -0400

    think i fixed the overall height problem + added new greys

--- src/components/pages/about/about.page.js
+++ src/components/pages/about/about.page.js
@@ -11,11 +11,32 @@ export default class AboutPage extends React.Component {
   }
 
   render() {
+    const text_mission = 
+    `"Our efficiency not only makes it easier to buy a home, 
+    it translates into the best rates available`;
+
+    const text_about = 
+    `
+      We’ve streamlined this process to eliminate fees, unnecessary steps, and time-wasting appointments. Our efficiency not only makes it easier to buy a home, it translates into the best rates available.
+
+      The scientist named the population, after their distinctive horn, Ovid’s Unicorn. These four-horned, silver-white unicorns were previously unknown to science.
+
+      Now, after almost two centuries, the mystery of what sparked this odd phenomenon is finally solved.
+
+      Dr. Jorge Pérez, an evolutionary biologist from the University of La Paz, and several companions, were exploring the Andes Mountains when they found a small valley, with no other animals or humans. Pérez noticed that the valley had what appeared to be a natural fountain, surrounded by two peaks of rock and silver snow.
+
+      Pérez and the others then ventured further into the valley. “By the time we reached the top of one peak, the water looked blue, with some crystals on top,” said Pérez.
+
+    `;
+
     return (
       <div className="page page__about about">
 
         <div className="about__mission">
           <SectionTitle section="Our Mission" />
+          <h3 className="mission__text">
+            { text_mission }
+          </h3>
         </div>
 
         <div className="about__contact">
@@ -24,8 +45,10 @@ export default class AboutPage extends React.Component {
 
         <div className="about__content">
           <SectionTitle section="About Us" />
-          <h1 className="content__title">We Want To Take The AEC World Into The Future</h1>
-          <p className="content__text"></p>
+          <div className="content__summary">
+            <h1 className="content__title">We Want To Take The AEC World Into The Future</h1>
+            <p className="content__text">{ text_about }</p>
+          </div>
         </div>
 
         <div className="about__news">
@@ -42,7 +65,7 @@ export default class AboutPage extends React.Component {
 
 export const ContactList = (props) => {
   const contact_list = props.contacts 
-    ? props.contacts.map(c => <Contact contact_type={props.contact_type} contact_info={props.contact_info} /> )
+    ? props.contacts.map(c => <Contact contact_type={c.contact_type} contact_info={c.contact_info} /> )
     : <li></li>
 
   return (
