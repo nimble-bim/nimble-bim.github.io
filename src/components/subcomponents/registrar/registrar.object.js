62c73d8c2dd90705630dce1589edbc21c3174fa0
PrestonII <smith.preston2@gmail.com>
2019-08-20 17:26:15 -0400

    added default styles to button & set up info

--- src/components/subcomponents/registrar/registrar.object.js
+++ src/components/subcomponents/registrar/registrar.object.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import './registrar.style.scss';
+import { RoundedButton } from '../buttons/buttons.object';
+import './registrar.desktop.scss';
+import './registrar.mobile.scss';
 
 export default class Registrar extends React.Component {
   constructor(props) {
@@ -10,16 +12,23 @@ export default class Registrar extends React.Component {
   render() {
     return (
       <div className="registrar">
+        <h4 className="registrar__title">Information</h4>
 
         <div className="registrar__info">
-          <div className="info__firstname"></div>
-          <div className="info__lastname"></div>
-          <div className="info__email"></div>
+          <div className="info__firstname">
+            <input type="text" placeholder="First Name" />
+          </div>
+          <div className="info__lastname">
+            <input type="text" placeholder="Last Name" />
+          </div>
+          <div className="info__email">
+            <input type="email" placeholder="Email" />  
+          </div>
         </div>
         
         <div className="registrar__button">
-          <span className="button__register">Register</span>
-          <span className="button__cancel">Cancel</span>
+          {/* <span className="button__register">Register</span> */}
+          <RoundedButton text="Register" modifier="register" />
         </div>
 
       </div>
