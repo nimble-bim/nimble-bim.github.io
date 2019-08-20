0fe1c6c24187765b7d264c1950c0f62c1e94b42e
PrestonII <smith.preston2@gmail.com>
2019-08-20 16:30:16 -0400

    added spacing to blurbs

--- src/components/subcomponents/blurb/blurb.object.js
+++ src/components/subcomponents/blurb/blurb.object.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import './blurb.style.scss';
 
-const Blurb = (props) => (
+const Blurb = (props) => {
+  return props.blurb_subtitle 
+    ? <SubtitledBlurb { ...props } />
+    : <UntitledBlurb {...props} />
+}
+
+
+const SubtitledBlurb = props => (
   <div className="blurb">
     <p className="blurb_subtitle">{props.blurb_subtitle}</p>
+    <h1 className="blurb_title">{props.blurb_title}</h1>
+    <p className="blurb_content">{props.blurb_content}</p>
+  </div>
+)
+
+const UntitledBlurb = props => (
+  <div className="blurb">
+    <p className="blurb_subtitle_empty"></p>
     <h1 className="blurb_title">{props.blurb_title}</h1>
     <p className="blurb_content">{props.blurb_content}</p>
   </div>
