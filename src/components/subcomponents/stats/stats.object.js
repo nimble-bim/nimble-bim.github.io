7a7be85cfdeb4f5f7fda766608dc41cd15ab2320
PrestonII <smith.preston2@gmail.com>
2019-08-21 08:23:33 -0400

    added keys to Stats

--- src/components/subcomponents/stats/stats.object.js
+++ src/components/subcomponents/stats/stats.object.js
@@ -3,7 +3,8 @@ import './stats.style.scss';
 import Stat from './stat.object';
 
 const Stats = props => {
-  var list = props.info ? props.info.map(s => <Stat number={s.number} description={s.desc}/> ) : <li></li>;
+  var i = 0;
+  var list = props.info ? props.info.map(s => <Stat key={i++} number={s.number} description={s.desc}/> ) : <li></li>;
 
   return (
     <div className="stats">
