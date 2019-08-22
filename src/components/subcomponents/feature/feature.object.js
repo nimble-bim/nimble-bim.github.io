12cba4614923545992c7e9909260c02859d02ccf
PrestonII <smith.preston2@gmail.com>
2019-08-22 16:17:07 -0400

    organized the mobile structure of the hiw page

--- src/components/subcomponents/feature/feature.object.js
+++ src/components/subcomponents/feature/feature.object.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import Blurb from '../blurb/blurb.object';
+import './feature.desktop.scss'
+import './feature.mobile.scss'
 
 const Feature = props => {
   const { fNumber, subtitle, title, content } = props.feature;
 
   return (
     <div className="feature">
+      <div className="feature__image"></div>
       <div className="feature__number">{ fNumber }</div>
-      <div className="feature__content">
-
-        <div className="content__summary">
-          <Blurb 
-            blurb_subtitle={ subtitle }
-            blurb_title={ title }
-            blurb_content={ content }
-          />
-        </div>
-        <div className="content__image"></div>
-        <div className="content__next"></div>
-
+      <div className="feature__summary">
+        <Blurb 
+          blurb_subtitle={ subtitle }
+          blurb_title={ title }
+          blurb_content={ content }
+        />
+      </div>
+      <div className="feature__next">
+        <p>Next</p>
       </div>
       <div className="feature__navigation"></div>
     </div>
