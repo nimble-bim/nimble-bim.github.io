fd407eee5cb26065c4f904f0d07d056c2f4335b2
Preston Smith (MARGIE_IV) <smith.preston2@gmail.com>
2020-05-02 20:01:35 -0400

    connected links in about page

--- src/components/subcomponents/contacts/contacts.object.js
+++ src/components/subcomponents/contacts/contacts.object.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import './contacts.style.scss';
 
-const Contact = (props) => (
-  <ul className="contact">
-    <li className="contact__type">{props.contact_type}</li>
-    <li className="contact__info">{props.contact_info}</li>
-  </ul>
-)
+const Contact = (props) => {
+  const { contact_type, contact_info, contact_link } = props;
+  const link = formatLink(contact_type, contact_link);
+
+  console.log(link);
+  return (
+    <ul className="contact">
+      <li className="contact__type">{ contact_type }</li>
+      <a href={ link } target="_blank">
+        <li className="contact__info">{ contact_info }</li>
+      </a>
+    </ul>
+  )
+}
 
 export const ContactList = (props) => {
   const contact_list = props.contacts 
-    ? props.contacts.map(c => <Contact contact_type={c.contact_type} contact_info={c.contact_info} /> )
+    ? props.contacts.map(c => <Contact contact_type={c.contact_type} contact_info={c.contact_info} contact_link={c.contact_link} /> )
     : <li></li>
 
   return (
@@ -20,4 +28,26 @@ export const ContactList = (props) => {
       </ul>
     </div>
   )
+}
+
+function formatLink(type, info) {
+  let link = '';
+  switch(type) {
+    case 'Email':
+      link = `mailto:${info}`;
+      break;
+
+    case 'Instagram':
+      link = `${info}`;
+      break;
+
+    case 'Twitter':
+      link = `${info}`;
+      break;
+
+    default:
+      break;
+  }
+
+  return link;
 }
